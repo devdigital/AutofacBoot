34c8e1ad5f133d8523a8023e2839af2b9f4bb053
devdigital <devdigital.cloud@gmail.com>
2017-11-10 14:49:46 +0000

    Fix/logger factory exception handler (#9)
    * fix: add loggerfactory to exception handler
    
    * chore: removes unused usings
    
    * chore: removes unused type

--- Source/AutofacBoot.Sample.Api/Bootstrap/ApplicationBootstrapTask.cs
+++ Source/AutofacBoot.Sample.Api/Bootstrap/ApplicationBootstrapTask.cs
@@ -1,6 +1,5 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 
--- Source/AutofacBoot/AutofacBootStartup.cs
+++ Source/AutofacBoot/AutofacBootStartup.cs
@@ -6,11 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace AutofacBoot
 {
     public class AutofacBootStartup : IStartup
     {
+        private readonly ILoggerFactory loggerFactory;
+
         private readonly IHostingEnvironment hostingEnvironment;
 
         private readonly AutofacStartupHelper startupHelper;
@@ -18,6 +21,7 @@ namespace AutofacBoot
         private readonly IContainerConfiguration containerConfiguration;
 
         public AutofacBootStartup(
+            ILoggerFactory loggerFactory,
             IHostingEnvironment environment, 
             IAutofacBootTaskResolver taskResolver,
             IContainerConfiguration containerConfiguration)
@@ -27,6 +31,7 @@ namespace AutofacBoot
                 throw new ArgumentNullException(nameof(taskResolver));
             }
 
+            this.loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
             this.hostingEnvironment = environment ?? throw new ArgumentNullException(nameof(environment));
             this.startupHelper = new AutofacStartupHelper(taskResolver);
             this.Configuration = this.startupHelper.Configuration(environment);
@@ -39,30 +44,43 @@ namespace AutofacBoot
 
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            this.startupHelper.ConfigureServices(services);
+            try
+            {
+                this.startupHelper.ConfigureServices(services);
 
-            var builder = new ContainerBuilder();            
-            builder.Populate(services);
+                var builder = new ContainerBuilder();
+                builder.Populate(services);
 
-            this.startupHelper.ConfigureContainer(builder, this.Configuration);
+                this.startupHelper.ConfigureContainer(builder, this.Configuration);
 
-            containerConfiguration.Configure(this.hostingEnvironment, builder);
+                containerConfiguration.Configure(this.hostingEnvironment, builder);
 
-            this.ApplicationContainer = builder.Build();
-            return new AutofacServiceProvider(this.ApplicationContainer);
+                this.ApplicationContainer = builder.Build();
+                return new AutofacServiceProvider(this.ApplicationContainer);
+            }
+            catch (Exception exception)
+            {
+                throw new AutofacBootException(this.loggerFactory, exception);
+            }
         }
 
-        public void Configure(
-            IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app)
         {
-            var bootstrapTasks = app.ApplicationServices
-                .GetServices(typeof(IApplicationBootstrapTask))
-                .Cast<IApplicationBootstrapTask>();
+            try
+            {
+                var bootstrapTasks = app.ApplicationServices
+                    .GetServices(typeof(IApplicationBootstrapTask))
+                    .Cast<IApplicationBootstrapTask>();
 
-            this.startupHelper.Configure(app, bootstrapTasks);
+                this.startupHelper.Configure(app, bootstrapTasks);
 
-            var appLifetime = app.ApplicationServices.GetService(typeof(IApplicationLifetime)) as IApplicationLifetime;
-            appLifetime?.ApplicationStopped.Register(() => this.ApplicationContainer.Dispose());
+                var appLifetime = app.ApplicationServices.GetService(typeof(IApplicationLifetime)) as IApplicationLifetime;
+                appLifetime?.ApplicationStopped.Register(() => this.ApplicationContainer.Dispose());
+            }
+            catch (Exception exception)
+            {
+                throw new AutofacBootException(this.loggerFactory, exception);
+            }       
         }
     }
 }
