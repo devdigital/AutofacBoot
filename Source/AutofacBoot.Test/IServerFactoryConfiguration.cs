baec0c9e5a5f0585476cb8b0602926de0bd5865e
devdigital <devdigital.cloud@gmail.com>
2018-07-27 09:55:24 +0100

    feat: add server configuration

---
+++ Source/AutofacBoot.Test/IServerFactoryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace AutofacBoot.Test
+{
+    /// <summary>
+    /// Server factory configuration.
+    /// </summary>
+    /// <typeparam name="TServerFactory">The type of the server factory.</typeparam>
+    public interface IServerFactoryConfiguration<in TServerFactory>
+    {
+        /// <summary>
+        /// Configures the specified factory.
+        /// </summary>
+        /// <param name="factory">The factory.</param>
+        void Configure(TServerFactory factory);
+    }
+}
