0a4933e00d07edcbd4c8b97c38a26ad235836386
devdigital <devdigital.cloud@gmail.com>
2018-08-13 14:53:37 +0100

    feat: add container configuration

---
+++ Source/AutofacBoot.Test/CompositeContainerConfiguration.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="CompositeContainerConfiguration.cs" company="DevDigital">
+// Copyright (c) DevDigital. All rights reserved.
+// </copyright>
+
+namespace AutofacBoot.Test
+{
+    using System;
+    using System.Threading.Tasks;
+    using Autofac;
+    using Microsoft.AspNetCore.Hosting;
+
+    /// <summary>
+    /// Composite container configuration.
+    /// </summary>
+    /// <seealso cref="AutofacBoot.IContainerConfiguration" />
+    public class CompositeContainerConfiguration : IContainerConfiguration
+    {
+        private readonly IContainerConfiguration[] containerConfigurations;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompositeContainerConfiguration"/> class.
+        /// </summary>
+        /// <param name="containerConfigurations">The container configurations.</param>
+        public CompositeContainerConfiguration(params IContainerConfiguration[] containerConfigurations)
+        {
+            this.containerConfigurations = containerConfigurations ??
+                throw new ArgumentNullException(nameof(containerConfigurations));
+        }
+
+        /// <inheritdoc />
+        public async Task Configure(IHostingEnvironment environment, ContainerBuilder builder)
+        {
+            foreach (var configuration in this.containerConfigurations)
+            {
+                if (configuration == null)
+                {
+                    continue;
+                }
+
+                await configuration.Configure(environment, builder);
+            }
+        }
+    }
+}
